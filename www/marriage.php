ad26f0937c1c5a968d62a2add091bcc0aac76833
barsuk <brudershaft@mail.ru>
2022-04-19 23:07:10 +0300

    WIP: select best currently girls

--- www/marriage.php
+++ www/marriage.php
@@ -1,5 +1,43 @@
 <?php
 
+class Woman {
+    public int $id;
+    public string $name;
+
+    public function __toString() {
+        return sprintf("Woman #%s, name %s\n", $this->id, $this->name);
+    }
+}
+
+class FreeMan {
+    public int $id;
+    public string $name;
+    public object $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function __toString() {
+        return sprintf("Man #%s, name %s\n", $this->id, $this->name);
+    }
+
+    // returns the best non-proposed woman
+    public function getBestWoman() {
+        $sth = $this->db->query("SELECT * FROM woman WHERE id = (
+                SELECT woman_id FROM priority WHERE man_id = $this->id AND weight =  
+                    (SELECT max(weight) FROM priority WHERE man_id = $this->id and is_proposed = 0)
+            )"
+        );
+
+        $woman = $sth->fetchObject("Woman");
+
+        return $woman;
+    }
+
+
+}
+
 $db_path = './human.sqlite';
 if (!file_exists($db_path)) 
     die(sprintf('there is no file at %s', $db_path));
@@ -8,14 +46,18 @@ $pdo = new PDO(
     "sqlite:$db_path",
     null,
     null,
-    array(PDO::ATTR_PERSISTENT => true)
+    [PDO::ATTR_PERSISTENT => true]
 );
 
-$sth = $pdo->query('SELECT * from woman');
-$rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+$sth = $pdo->query('SELECT * from man');
+$free_men = [];
 
-foreach($rows as $k => $v) {
-    printf("%s is %s\n", $k, json_encode($v));
+while($man = $sth->fetchObject("FreeMan", [$pdo])) {
+    $free_men[] = $man;
 }
 
-unset($pdo);
+while(!empty($free_men)) {
+    $man = array_pop($free_men);
+
+    echo $man, " and his ", $man->getBestWoman();
+}
